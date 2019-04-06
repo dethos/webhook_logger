f2c2eab75cf8dffdb449f617688ce9f35ad27fbf
Gonçalo Valério <gon@ovalerio.net>
2019-04-06 13:21:03 +0100

    fix get_ip_address

--- callbacks/utils.py
+++ callbacks/utils.py
@@ -2,8 +2,8 @@ from django.http import HttpRequest
 
 
 def get_ip_address(request: HttpRequest) -> str:
-    forwarded = request.META.get("X-FORWARDED-FOR")
+    forwarded = request.headers.get("X-Forwarded-For")
     if forwarded:
-        return forwarded.split(",")[0]
+        return forwarded.split(",")[-1]
     else:
         return request.META.get("REMOTE_ADDR")
